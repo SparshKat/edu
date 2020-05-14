69360b8da6001f23c27110aff5a7afae260bfe9f
Sparsh Katiyar <sparshkat99@gmail.com>
2020-05-14 17:33:30 +0530

    Window Sliders added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Row, Nav, Button, Container, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HeaderPage from "./builtComponents/HeaderPage"
+import "./assets/custom.scss"
 
+import HeaderPage from "./builtComponents/HeaderPage"
+import CarouselPage2 from './builtComponents/CarouselPage2';
+import WindowSlider from './builtComponents/WindowSlider';
 function App() {
   return (
     <>
-    <HeaderPage />
-    <Container>
+      <HeaderPage />
+      <Container>
+        <WindowSlider />
+       <CarouselPage2 />
+      </Container>
+
       
-    </Container>
     </>
   );
 }
---
+++ src/builtComponents/CarouselPage2.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Row, Nav, Button, Container, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactPlayer from "react-player"
+// import HeaderPage from "./builtComponents/HeaderPage"
+import "../assets/custom.scss"
+
+function CarouselPage2() {
+  return (
+    <>
+      
+      <Container>
+        <Row>
+          <Col md="5" sm="5">
+            <h2>Sudama Prasad Singh , BDO</h2>
+            <h3 className="Subtitle">BPSC 63 Interview Class</h3>
+            <p>
+            Sudama Prasad Singh sir's lecture on BDO for BPSC 63 Interview
+            </p>
+            <Button>JOIN CLASSES</Button>
+          </Col>
+          <Col md="1" sm="1">
+          </Col>
+          <Col md="5" sm="5">
+            <div className='player-wrapper'>
+              <   ReactPlayer
+                className='react-player'
+                width='100%'
+                height='100%'
+                url="https://www.youtube.com/watch?v=OrYsn4WuyZ4"
+              />
+            </div>
+
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default CarouselPage2;

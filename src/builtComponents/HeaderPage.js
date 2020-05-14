0369fde5ea72ce30f85862592ab96ddd60bbfc94
Sparsh Katiyar <sparshkat99@gmail.com>
2020-05-14 16:45:26 +0530

    Header updated

---
+++ src/builtComponents/HeaderPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Row, Nav, FormControl, Button, Form, Container, Col, Navbar } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Sarthak Samwad</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Blog</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+          <Form inline>
+            {/* <FormControl type="text" placeholder="Search" className=" mr-sm-2" /> */}
+            <Button className="btn btn-danger" type="submit">Join Classes</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+
+    </Navbar>
+
+  );
+}
+
+export default Header;

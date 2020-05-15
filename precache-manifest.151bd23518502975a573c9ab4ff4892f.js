09fbba2f41459962e44fbd41fce859913c8b90d4
Sparsh Katiyar <sparshkat99@gmail.com>
2020-05-15 19:02:59 +0530

    Updates

---
+++ precache-manifest.151bd23518502975a573c9ab4ff4892f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970709771c6ecdd4c543e8a19c9cf208",
+    "url": "/edu/index.html"
+  },
+  {
+    "revision": "1698cb5634751dc1f9a7",
+    "url": "/edu/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cd196afde945c5e6db4d",
+    "url": "/edu/static/css/main.321ccb05.chunk.css"
+  },
+  {
+    "revision": "1698cb5634751dc1f9a7",
+    "url": "/edu/static/js/2.c16e25cb.chunk.js"
+  },
+  {
+    "revision": "1adb110813a30163ca679a14e0de12c3",
+    "url": "/edu/static/js/2.c16e25cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd196afde945c5e6db4d",
+    "url": "/edu/static/js/main.5d5ceb32.chunk.js"
+  },
+  {
+    "revision": "a87cb4782b1bc5dadabf",
+    "url": "/edu/static/js/runtime-main.2d4760ef.js"
+  },
+  {
+    "revision": "8daf8ff8dc62c63ba4d1dc5636c53a44",
+    "url": "/edu/static/media/c1.8daf8ff8.jpg"
+  },
+  {
+    "revision": "b2662e082594a32f044f02b8120e950d",
+    "url": "/edu/static/media/c2.b2662e08.jpg"
+  },
+  {
+    "revision": "f48a732d94512ce16a956a56827bd04c",
+    "url": "/edu/static/media/c3.f48a732d.jpg"
+  },
+  {
+    "revision": "3b0a01ec431a18f6ad73ce467b85e0fa",
+    "url": "/edu/static/media/c4.3b0a01ec.jpg"
+  },
+  {
+    "revision": "2d1f4a16b1e8e9ef5634e7399e12effb",
+    "url": "/edu/static/media/c5.2d1f4a16.jpg"
+  },
+  {
+    "revision": "e9d407048efe5943ae27d66055b6ed15",
+    "url": "/edu/static/media/c6.e9d40704.jpg"
+  },
+  {
+    "revision": "b910dfc1ea4934dfe15c4acfb9fd688e",
+    "url": "/edu/static/media/iPhone.b910dfc1.png"
+  },
+  {
+    "revision": "ab37af043c04f533ba966e5d512abed5",
+    "url": "/edu/static/media/notif1.ab37af04.jpeg"
+  },
+  {
+    "revision": "9cb95ab422d935e85be8980c4d89ac3d",
+    "url": "/edu/static/media/notif3.9cb95ab4.jpeg"
+  },
+  {
+    "revision": "f2f6f2b3517da357126b0789403f03bd",
+    "url": "/edu/static/media/playStore.f2f6f2b3.png"
+  },
+  {
+    "revision": "33c12eb888ec040f960dccd6787f6d6b",
+    "url": "/edu/static/media/window1.33c12eb8.png"
+  },
+  {
+    "revision": "2766bd558ae686dc58fb8296eade0655",
+    "url": "/edu/static/media/window2.2766bd55.jpg"
+  }
+]);
